a788d7c23c1b9c550d24989600fc5cdffc460ec5
Shad Storhaug <shad@shadstorhaug.com>
2026-09-24 00:44:37 +0700

    Added license headers for ArrayBufferWriter tests

--- tests/Morfologik.Stemming.Tests/Support/ArrayBufferWriterTests.T.cs
+++ tests/Morfologik.Stemming.Tests/Support/ArrayBufferWriterTests.T.cs
@@ -1,4 +1,7 @@
-﻿using Morfologik.TestFramework;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Morfologik.TestFramework;
 using NUnit.Framework;
 using System;
 using System.Buffers;
