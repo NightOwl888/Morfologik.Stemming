ac3f81492bd57d5d8df2c11db70987a07e473a3d
Shad Storhaug <shad@shadstorhaug.com>
2026-09-24 00:36:31 +0700

    DictionaryMetadata: Removd unused usings

--- src/Morfologik.Stemming/DictionaryMetadata.cs
+++ src/Morfologik.Stemming/DictionaryMetadata.cs
@@ -1,12 +1,10 @@
 ﻿using J2N;
-using J2N.Collections.Generic.Extensions;
 using J2N.Text;
 using Morfologik.Stemming.Support;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Text;
 using JCG = J2N.Collections.Generic;
 
